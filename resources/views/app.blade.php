bbc04ac7570c3fd85f499482dc1020d529489b9b
dadcog-hYvpan-kipro0 <rickvisser@MacBook-Pro-van-Rick.local>
2024-05-17 17:05:36 +0200

    :lipstick: Added color to body

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,7 +15,7 @@
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class="antialiased bg-gray-100">
         @inertia
     </body>
 </html>
